fb3178942be57496cdb9317dc2fd1591f417f8ed
Jose Vergara <117672114+Josveco123@users.noreply.github.com>
2023-06-20 17:06:19 -0500

    version versio 22

--- src/App.js
+++ src/App.js
@@ -62,21 +62,6 @@ function anteriorSlide() {
 
 mostrarSlide();
 
-/*
-
-desplegar o contraer busqueda
-
-*/
-
-
-
-
-
-/*
-
-gererar hoja de contactos
-
-*/
 
 
 function contactos() {
@@ -86,155 +71,87 @@ function contactos() {
   // Crear un elemento div para contener el fragmento HTML
   contacto.innerHTML = `
   <section id="pagina4">
-  <h2 class="tit-nos-pag4">CONTACTOS</h2>
+  <h2 class="tit-nos-pag4">CONTACTOS
+  </h2>
   <div id="linea-horizontal4"></div>
   <br />
-  <!-- pagina 4.1 -->
-  <div id="direcciones">
-    <div id="direcciones-I">
-      <h2 class="tit-pag4 pag4-I">CUENTAS CORRIENTES</h2>
-      <p>
-        <span>BANCO DE COLOMBIA</span><br />
-        <span>50604761202</span><br />
-        <span>BANCO BBVA</span><br />
-        <span>826007817 NIT: 892200917-3</span>
-      </p>
-      <br />
-      <h2 class="tit-pag4 pag4-I">LISTA DE PRECIOS 2017</h2>
-      <p>
-        <span><strong> Descarga aquí</strong></span
-        ><br />
-      </p>
-      <br />
-      <h2 class="tit-pag4 pag4-I">FORMATO DE PEDIDO</h2>
-      <p>
-        <span><strong> Descarga aquí</strong></span
-        ><br />
-      </p>
+  <div id="contactoPag">
+    <div id="direcciones">
+      <h2 class="tit-pag4">INFORMACIÓN POR ZONAS</h2>
+      <div id="ContactoCorreos">
+        <p>
+          <span><strong>SINCELEJO, SUCRE</strong></span><br />
+          <span>CELULAR: (+57) 3135120781</span><br />
+          <span>E-mail: sincelejo@laboratoriodelcaribe.com</span>
+        </p>
+        <p>
+          <span><strong>BARRANQUILLA, ATLÁNTICO</strong></span><br />
+          <span>CELULAR: (+57) 3126873831</span><br />
+          <span>E-mail:</span><br />
+          <span>barranquilla@laboratoriodelcaribe.com</span><br />
+          <span>barranquilla1@laboratoriodelcaribe.com</span>
+        </p>
+        <p>
+          <span><strong>MONTERÍA, CÓRDOBA</strong></span><br />
+          <span>CELULAR: (+57) 3126874903</span><br />
+          <span>E-mail: monteria@laboratoriodelcaribe.com</span>
+        </p>
+        <p>
+          <span><strong>BUCARAMANGA, SANTANDER</strong></span><br />
+          <span>CELULAR: (+57) 3126874922</span><br />
+          <span>Email: bucaramanga@laboratoriodelcaribe.com</span>
+        </p>
+        <p>
+          <span><strong>CARTAGENA, BOLIVAR</strong></span><br />
+          <span>CELULAR: (+57) 3216987967</span><br />
+          <span>E-mail: cartagena@laboratoriodelcaribe.com</span>
+        </p>
+        <p>
+          <span><strong>VALLEDUPAR, CESAR</strong></span><br />
+          <span>CELULAR: (+57) 3126703794</span><br />
+          <span>E-mail: valledupar@laboratoriodelcaribe.com</span>
+        </p>
+        <p>
+          <span><strong>CÚCUTA, NORTE DE SANTANDER</strong></span><br />
+          <span>CELULAR: (+57) 3145247471</span><br />
+          <span>E-mail: cucuta@laboratoriodelcaribe.com</span>
+        </p>
+      </div>
     </div>
-    <div id="direcciones-D">
-      <h2 class="tit-pag4 pag4-D">INFORMACIÓN POR ZONAS</h2>
-      <div id="zonaID">
-        <div id="zonaI">
-          <p>
-            <span><strong>SINCELEJO, SUCRE</strong></span
-            ><br />
-            <span>CELULAR: (+57) 3135120781</span><br />
-            <span>E-mail: sincelejo@laboratoriodelcaribe.com</span><br />
-          </p>
-          <p>
-            <span><strong>BARRANQUILLA, ATLÁNTICO</strong></span
-            ><br />
-            <span>CELULAR: (+57) 3126873831</span><br />
-            <span>E-mail:</span><br />
-            <span>barranquilla@laboratoriodelcaribe.com</span><br />
-            <span>barranquilla1@laboratoriodelcaribe.com</span><br />
-          </p>
-          <p>
-            <span><strong>MONTERÍA, CÓRDOBA</strong></span
-            ><br />
-            <span>CELULAR: (+57) 3126874903</span><br />
-            <span>E-mail: monteria@laboratoriodelcaribe.com</span><br />
-          </p>
-          <p>
-            <span><strong>BUCARAMANGA, SANTANDER</strong></span
-            ><br />
-            <span>CELULAR: (+57) 3126874922</span><br />
-            <span>Email: bucaramanga@laboratoriodelcaribe.com</span><br />
-          </p>
-        </div>
-        <div id="zonaD">
-          <p>
-            <span><strong>CARTAGENA, BOLIVAR</strong></span
-            ><br />
-            <span>CELULAR: (+57) 3216987967</span><br />
-            <span>E-mail: cartagena@laboratoriodelcaribe.com</span><br />
-          </p>
-          <p>
-            <span><strong>VALLEDUPAR, CESAR</strong></span
-            ><br />
-            <span>CELULAR: (+57) 3126703794</span><br />
-            <span>E-mail: valledupar@laboratoriodelcaribe.com</span><br />
-          </p>
-          <p>
-            <span><strong>CÚCUTA, NORTE DE SANTANDER</strong></span
-            ><br />
-            <span>CELULAR: (+57) 3145247471</span><br />
-            <span>E-mail: cucuta@laboratoriodelcaribe.com</span><br />
-          </p>
-        </div>
+    <div id="correos">
+      <h2 class="tit-pag4D">Queremos escucharte!</h2>
+      <div id="correosPag">
+        <form id="formulario-correo" action="procesar_formulario.php" method="POST">
+          <label class="label-correo" for="nombre">Nombre:</label>
+          <input type="text" id="nombre" name="nombre" required />
+
+          <label class="label-correo" for="correo">Correo:</label>
+          <input type="email" id="correo" name="correo" required />
+
+          <label class="label-correo" for="telefono">Teléfono:</label>
+          <input type="tel" id="telefono" name="telefono" required />
+
+          <label class="label-correo" for="mensaje">Mensaje:</label>
+          <textarea id="mensaje" name="mensaje" rows="4" required></textarea><br /><br />
+
+          <input type="submit" value="Enviar" />
+        </form>
       </div>
     </div>
   </div>
-  <!-- pagina 4.2 -->
-  <div id="correos">
-    <div id="correosI">
-      <h2 class="tit-pag4">CORREOS DE LA COMPAÑÍA</h2>
-      <p>
-        administrativo@laboratoriodelcaribe.com
-        barranquilla@laboratoriodelcaribe.com
-        barranquilla1@laboratoriodelcaribe.com
-        barranquilla2@laboratoriodelcaribe.com
-        bucaramanga@laboratoriodelcaribe.com
-        bucaramanga1@laboratoriodelcaribe.com
-        cartagena@laboratoriodelcaribe.com
-        comercial@laboratoriodelcaribe.com
-        contabilidad@laboratoriodelcaribe.com
-        cucuta@laboratoriodelcaribe.com
-        direcciontecnica@laboratoriodelcaribe.com
-        gerencia@laboratoriodelcaribe.com
-        gerenciaventas@laboratoriodelcaribe.com
-        monteria@laboratoriodelcaribe.com
-        sincelejo@laboratoriodelcaribe.com
-        sincelejo1@laboratoriodelcaribe.com
-        valledupar@laboratoriodelcaribe.com
-        ventas@laboratoriodelcaribe.com comercial@laboratoriodelcaribe.com
-      </p>
-    </div>
-    <div id="correosD">
-      <h2 class="tit-pag4D">Queremos escucharte!</h2>
 
-      <form
-        id="formulario-correo"
-        action="procesar_formulario.php"
-        method="POST"
-      >
-        <label class="label-correo" for="nombre">Nombre:</label>
-        <input type="text" id="nombre" name="nombre" required />
-
-        <label class="label-correo" for="correo">Correo:</label>
-        <input type="email" id="correo" name="correo" required />
-
-        <label class="label-correo" for="telefono">Teléfono:</label>
-        <input type="tel" id="telefono" name="telefono" required />
-
-        <label class="label-correo" for="mensaje">Mensaje:</label>
-        <textarea id="mensaje" name="mensaje" rows="4" required></textarea
-        ><br /><br />
-
-        <input type="submit" value="Enviar" />
-      </form>
-    </div>
-  </div>
-  <!-- pagina 4.3 -->
   <div id="gogle-map">
     <h2 id="tit-map">laboratorio del Caribe</h2>
-    <iframe
-      id="mapa-map"
+    <iframe id="mapa-map"
       src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1968.8334407712973!2d-75.41080065071586!3d9.274028265514518!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8e5914ed96a19587%3A0x476cf87d89af0f53!2sLABORATORIO%20DEL%20CARIBE!5e0!3m2!1sen!2sco!4v1684946281250!5m2!1sen!2sco"
-      width="100%"
-      height="450"
-      style="border: 0"
-      allowfullscreen=""
-      loading="lazy"
-      referrerpolicy="no-referrer-when-downgrade"
-    ></iframe>
+      width="100%" height="450" style="border: 0" allowfullscreen="" loading="lazy"
+      referrerpolicy="no-referrer-when-downgrade"></iframe>
   </div>
-  </section>
+</section>
 `;
 
   // Insertar el fragmento HTML dentro del contenedor
-  document.getElementById("flecha").style.display="block";
+  document.getElementById("flecha").style.display = "block";
 }
 
 /**
@@ -246,7 +163,7 @@ function contactos() {
 
 async function fNosotros() {
   await functionNosotros();
-  document.getElementById("flecha").style.display="block";
+  document.getElementById("flecha").style.display = "block";
 }
 
 function functionNosotros() {
@@ -399,72 +316,69 @@ function functionNosotros() {
 function producto() {
   var producto = document.querySelector("#cuerpo");
   producto.innerHTML = `
-  <div id="contenedor-producto">
-  <div id="cont-producto">
-    <div id="tit1">
-      <h1>Ingresa tu cuenta</h1>
-    </div>
+    <div id="contenedor-producto">
+      <div id="cont-producto">
+        <div id="tit1">
+          <h1>Ingresa tu cuenta</h1>
+        </div>
 
-    <form id="datos-captura" action="">
-      <div id="E-mail" class="datos-correo">
-        <p>Email Empresa o Persona</p>
-        <input
-          id="correo"
-          class="correo"
-          type="text"
-          placeholder="Correo electrónico"
-        />
-      </div>
+        <form id="datos-captura" action="">
+          <div id="E-mail" class="datos-correo">
+            <p>Email Empresa o Persona</p>
+            <input
+            id="correo"
+            class="correo"
+            type="text"
+            placeholder="Correo electrónico"
+            />
+          </div>
 
-      <div id="contraseña" class="datos-correo">
-        <p>Contraseña de acceso</p>
-        <input
-          id="clave"
-          class="clave"
-          type="text"
-          placeholder="Contraseña"
-        />
-      </div>      
-    </form>
-    <div id="ejecutar">
-      <button id="boton-registro" onclick="GETregistro()">
-        Verificar
-      </button>
-      <br>
-      <a id="ocultar" href="/index.html">Exit</a>
+          <div id="contraseña" class="datos-correo">
+            <p>Contraseña de acceso</p>
+            <input
+            id="clave"
+            class="clave"
+            type="text"
+             placeholder="Contraseña"
+            />
+          </div>      
+        </form>
+           <div id="ejecutar">
+          <button id="boton-registro" onclick="GETregistro()">
+          Verificar
+          </button>
+          <a id="ocultar" href="/index.html">Exit</a>
+          <a id="olvido" href="#>Olvide mi Contraseña</a>
+        </div>
+     </div>
+     <p id="bienvenida">
+     Bienvenido a Laboratorios del caribe. <br />Su Cuenta Fue creada con
+      exito. <br /><br />
+      <a href="index.html">Click Continuar</a>
+      </p>
     </div>
-  </div>
-  <p id="bienvenida">
-    Bienvenido a Laboratorios del caribe. <br />Su Cuenta Fue creada con
-    exito. <br /><br />
-    <a href="index.html">Click Continuar</a>
-  </p>
-</div>
-`;
-document.getElementById("flecha").style.display="block";
+  `;
+  document.getElementById("flecha").style.display = "block";
 }
 
 function registro() {
   var registro = document.querySelector("body");
   registro.innerHTML += `
-  <div id="cuerpo">
-  <div id="contenedor-contacto">
-      <div id="cont-registro">
+    <div id="cuerpo">
+      <div id="contenedor-contacto">
+        <div id="cont-registro">
           <div id="tit1">
               <h1>Ingresa tu cuenta</h1>
           </div>
-
           <form id="datos-captura" action="">
               <div id="E-mail" class="datos-correo">
                   <p class="pCaptura">Email Empresa o Persona</p>
                   <input id="correo" class="correo" type="text" placeholder="Correo electrónico" />
               </div>
-
               <div id="contraseña" class="datos-correo">
                   <p class="pCaptura">Contraseña de acceso</p>
                   <input id="clave" class="clave" type="text" placeholder="Contraseña" />
               </div>
-
               <div id="razon-social" class="datos-correo">
                   <p class="pCaptura">Razon Social de la empresa</p>
                   <input id="razonSocial" class="razonSocial" type="text" placeholder="Razon Social" />
@@ -482,17 +396,17 @@ function registro() {
               <button id="boton-registro" onclick="POSTregistro()">
                   Registrarse
               </button>
-              <br>
-              <a id="ocultar" href="/index.html">Exit</a>
+               <a id="ocultar" href="/index.html">Exit</a>
           </div>
-      </div>
-      <p id="bienvenida">
+        </div>
+        <p id="bienvenida">
           Bienvenido a Laboratorios del Caribe. <br />Su cuenta ha sido creada
           con éxito. <br /><br />
           <a href="index.html">Continuar</a>
-      </p>
-  </div>
-</div>
+        </p>
+        </div>  
+      </div>
+    </div>
   `;
 }
 /*
@@ -523,13 +437,13 @@ function mostrar(mostrar, valor) {
 
 function cerrarAlerta(parametro1, parametro2) {
   if (parametro2 === 'GET') {
- 
+
     location.reload(true);
     // termina prueba
-};
+  };
   if (parametro2 === 'POST') {
     location.reload()
   }
   var alerta = document.querySelector('#cuerpo1');
-   alerta.parentNode.removeChild(alerta);
+  alerta.parentNode.removeChild(alerta);
 }
